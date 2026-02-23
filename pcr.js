cde818730956ce8e0415eb9809e391431337c005
Gunjan <gunjansamant46@gmail.com>
2026-02-24 02:20:11 +0530

    Add functions to show and hide content sections

--- pcr.js
+++ pcr.js
@@ -15,3 +15,23 @@ document.getElementById("demo").style.display = "none";
 document.getElementById("guided").style.display = "none";
 document.getElementById("free").style.display = "block";
 }
+function hideAll() {
+    document.getElementById("nephron").style.display = "none";
+    document.getElementById("genetic").style.display = "none";
+    document.getElementById("skeleton").style.display = "none";
+}
+
+function showNephron() {
+    hideAll();
+    document.getElementById("nephron").style.display = "block";
+}
+
+function showGenetic() {
+    hideAll();
+    document.getElementById("genetic").style.display = "block";
+}
+
+function showSkeleton() {
+    hideAll();
+    document.getElementById("skeleton").style.display = "block";
+}
